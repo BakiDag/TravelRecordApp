c3cdca9ea2a5422ffa1ed17d92901866ed0bd8ad
BakiDag <73464615+BakiDag@users.noreply.github.com>
2023-12-25 23:50:54 +0100

    [DEV] set first look of the app

--- TravelRecordApp/TravelRecordApp/MainPage.xaml.cs
+++ TravelRecordApp/TravelRecordApp/MainPage.xaml.cs
@@ -14,5 +14,10 @@ namespace TravelRecordApp
         {
             InitializeComponent();
         }
+
+        private void loginButton_Clicked(object sender, EventArgs e)
+        {
+
+        }
     }
 }
